170d30f56b7a6852122bf03b6c3cb96356e239a2
Zuev91 <zionix007@gmail.com>
2023-07-14 12:50:33 +0300

    detele tacking

--- Documents/Development/Alex Scutorenko Course/NewAttempt(28012023)/SwiftLesson01/SwiftLesson01.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-let firstName = "Ivan"
-let lastName = "Ivanov"
-let height = 183
-let weight = 106.6
-let age = 31
-print("First name: \(firstName) \nLast name: \(lastName) \nHeight: \(height) \nWeight: \(weight) \nAge: \(age)")
